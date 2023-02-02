48fdd957d658c0408c0fd48598e8ba5cd489b741
Fang Yuan <wojiushifangyuanlove@gmail.com>
2023-02-02 17:55:42 +0800

    update: study basic
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-    let string_append = String::from("hello ");
-    let string_rust = String::from("rust");
-    // &string_rust会自动解引用为&str
-    let result = string_append + &string_rust;
-    let mut result = result + "!";
-    result += "!!!";
-
-    println!("连接字符串 + -> {}", result);
+    let mut arr: [i128; 32 * 1024] = [0; 32 * 1024];
+    println!("{} {}", arr[0], arr[32 * 1024 - 1]);
+    arr[32 * 1024 - 1] = 12345;
+    println!("{} {}", arr[0], arr[32 * 1024 - 1]);
 }
