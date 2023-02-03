7ead49f7722a0dddbc3d49436025c8139d013a31
Fang Yuan <wojiushifangyuanlove@gmail.com>
2023-02-03 16:31:20 +0800

    update: study function
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut arr: [i128; 32 * 1024] = [0; 32 * 1024];
-    println!("{} {}", arr[0], arr[32 * 1024 - 1]);
-    arr[32 * 1024 - 1] = 12345;
-    println!("{} {}", arr[0], arr[32 * 1024 - 1]);
+    let x = nothing();
+    println!("{:?}", x);
+    stop();
 }
+
+fn stop() -> ! {
+    panic!("Must stop");
+}
+
+fn nothing() {}
